95a84b49840c32f78eaeec0516864ba35ff84902
SaewookYoo_home <l71811360@gmail.com>
2018-10-27 15:54:36 +0900

    problem happen

--- src/task2/Graph.java
+++ src/task2/Graph.java
@@ -3,23 +3,64 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class Graph {
 	// Member variable
 	private int num_vertices = 0;
-	private ArrayList<Vertex> adj_list = null;
-	double [][]adj_mat = null;
+	private Vector<Vertex> adj_list;
+	private double [][]adj_mat = null;
 	
 	// Constructor
 	public Graph(String FILE_PATH) {
-		MakeWeightMat(FILE_PATH);
-		
+		MakeheightMat(FILE_PATH);
+		MakeAdjList();
 	}
 	
 	// Method
+	public void MakeAdjList() {
+		adj_list = new Vector<Vertex>();
+		adj_list.setSize(num_vertices);
+		for (int i=0; i<num_vertices; i++) {
+			adj_list.setElementAt(null, i);
+			for (int j = 0; j<num_vertices; j++) {
+				if (adj_mat[i][j] != -1) {
+					Vertex new_vertex = new Vertex(i,j); // Vertex.id = (i,j)
+					new_vertex.setheight(adj_mat[i][j]);
+					new_vertex.next = null;
+					insertVertex(i, new_vertex);
+				}
+			}
+		}
+		
+	}	
+	public void insertVertex(int src_vertex, Vertex new_vertex) {
+		if (adj_list.get(src_vertex) == null) {
+			adj_list.setElementAt(new_vertex, src_vertex);
+		}
+		else {
+			Vertex curr = adj_list.get(src_vertex);
+			while(curr.next != null) curr = curr.next; // If curr.next == null, break 
+			curr.next = new_vertex;
+		}
+		
+	}
 	
+	public void showAdjList() {
+		for(int i=0; i<num_vertices; i++) {
+			Vertex curr = adj_list.get(i);
+			curr.printId();
+			System.out.print("'s adj_list : ");
+			while(curr != null) {
+				curr.printId();
+				System.out.print(": "+curr.getheight()+ " \u2192 ");
+				curr = curr.next;
+			}
+			System.out.println();
+		}
+	}
 	
-	public void MakeWeightMat(String FILE_PATH) {
+	public void MakeheightMat(String FILE_PATH) {
 		File f = null;
 		FileReader fr = null;
 		BufferedReader in_file = null;
--- src/task2/Vertex.java
+++ src/task2/Vertex.java
@@ -1,16 +1,24 @@
 package task2;
-
-class Point extends Vertex{
-	private int x;
-	private int y;
-	public Point(int _x, int _y) {
-		x = _x;
-		y = _y;
-	}
-}
+import javafx.util.Pair;
 
 public class Vertex {
-	private Point id;
-	private double weight;
+	private Pair<Integer, Integer> id;
+	private double height;
 	public Vertex next;
+	public Vertex(int _x, int _y) {
+		id = new Pair<Integer, Integer>(_x,_y);
+	}
+	public void setheight(double _height) {
+		height = _height;
+	}
+	public double getheight() {
+		return height;
+	}
+	public void printId() {
+		System.out.print("("+id.getKey()+","+id.getValue()+") ");
+	}
+	public  Pair<Integer, Integer> getId(){
+		return id;
+	}
+	
 }
